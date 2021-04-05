d9a7a0c6776de10b4ee2bfea6621c63601d6ef99
Alex <s-slobodyanik@mail.ru>
2021-04-05 23:01:41 +0300

    05 04 2021 nothing

---
+++ testing/js/in.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset='utf-8'>
+    <title>Чатики</title>
+    <link href='style.css' rel='stylesheet'>
+</head>
+<body>
+
+<div class="main">
+    <div class="little">Текст1</div>
+    <div class="little">Текст2</div>
+    <div class="little">Текст3</div>
+    <div class="little">Текст4</div>
+
+
+
+</div>
+</body>
+</html>
