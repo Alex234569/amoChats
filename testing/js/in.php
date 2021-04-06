71c0c7a3e66b459b192f8009b0c7f331ed227318
Александр <s-slobodyanik@mail.ru>
2021-04-06 18:55:02 +0300

    06 04 2021 nothing1

--- testing/js/in.php
+++ testing/js/in.php
@@ -4,17 +4,48 @@
     <meta charset='utf-8'>
     <title>Чатики</title>
     <link href='style.css' rel='stylesheet'>
+    <script src="http://code.jquery.com/jquery-2.0.2.min.js"></script>
+    <script src="script.js"></script>
 </head>
 <body>
-
+<!--
+    <div class="left">
+    <div class="right">
+-->
 <div class="main">
     <div class="little">Текст1</div>
     <div class="little">Текст2</div>
-    <div class="little">Текст3</div>
-    <div class="little">Текст4</div>
+
+
+    <!--
+        <div class="little">
+            Текст3
+            <a href="javascript:PopUpShow(1)">Show popup1</a>
+        </div>
+        <div class="popup" id="popup1">
+            <div class="popup-content">
+                Text in Popup1
+                <a href="javascript:PopUpHide(1)">Hide popup1</a>
+            </div>
+        </div>
+
+
+        <div class="little">
+            Sample Text
+            <a href="javascript:PopUpShow(2)">Show popup2</a>
+        </div>
+        <div class="popup" id="popup2">
+            <div class="popup-content">
+                Text in Popup2
+                <a href="javascript:PopUpHide(2)">Hide popup2</a>
+            </div>
+        </div>
+    -->
 
 
 
-</div>
-</body>
-</html>
+
+
+    </div>
+    </body>
+    </html>
---
+++ testing/js/script.js
@@ -0,0 +1,20 @@
+
+
+
+/*
+$(document).ready(function(){
+    PopUpHide();
+});
+
+function PopUpShow(num){
+    $("#popup"+num).show();
+}
+
+function PopUpHide(){
+    $("#popup1").hide();
+    $("#popup2").hide();
+}
+
+*/
+
+
